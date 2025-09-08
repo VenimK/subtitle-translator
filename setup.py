05b3a9da1ee04043508d2a90311bc4253f34f693
VenimK <venimkmac@gmail.com>
2025-09-08 07:55:53 +0200

    Fix dependency specifications and add GUI as optional dependency

--- setup.py
+++ setup.py
@@ -7,8 +7,16 @@ setup(
     install_requires=[
         "aiohttp>=3.8.0",
         "langdetect>=1.0.9",
-        "PyQt6>=6.0.0; 'gui' in sys.argv or 'gui' in sys_platform",
     ],
+    extras_require={
+        'gui': ['PyQt6>=6.0.0'],
+        'dev': [
+            'pytest>=6.0',
+            'black>=21.0',
+            'isort>=5.0',
+            'mypy>=0.900',
+        ],
+    },
     entry_points={
         'console_scripts': [
             'subtitle-translator=subtitle_translator.cli.main:main',
